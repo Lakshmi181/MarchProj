882b943114e7cd688eb1047420f7f6b0dc011f6d
Lakshmi181 <seleniumtambaram@gmail.com>
2021-03-31 02:04:33 +0530

    br2 added

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -5,5 +5,13 @@ public class Sample {
 	private void print() {
 		int a=10;
 		System.out.println(a);
+		
+		int b=20;
+		System.out.println(b);
+		
+		int c=a+b;
+		System.out.println(c);
+		
+		
 	}
 }
